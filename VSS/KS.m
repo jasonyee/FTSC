c9d7cb12d5cb6d49e695293ec3692abf765762ff
Kylin Yi <jialinyi.whu@gmail.com>
2017-07-16 21:46:47 -0400

    DSS
    -adding dss_uni, fme2dss, fme_dss_NlogLik, dss_NlogLik.
    -adding test for dss evaluation.
    -removing opti of KS
    
    -editing tests for KS&KF

--- VSS/KS.m
+++ VSS/KS.m
@@ -10,6 +10,9 @@ function KalmanFit = ...
 %   -MeasMX(:,:,t): the measurement matrix at t.
 %   -ObseCov(:,:,t): the observation innovation covariance matrix at t.
 %   -data(:,t) the dependent data at t.
+%   -StateMean0(:): the initial states, t=0.
+%   -StateCov0(:,:): the initial states covariance matrix, t=0.
+
 %Output: KalmanFit is a structure, for t=1:T
 %   -loglik: log-likelihood of dependent data 1:T
 %   -ForecastedMean: E(x_t|1:t-1)
@@ -18,12 +21,13 @@ function KalmanFit = ...
 %   -FilteredCov: Cov(x_t|1:t)
 %   -SmoothedMean: E(x_t|1:T)
 %   -SmoothedCov: Cov(x_t|1:T)
-%   -C0: C(0) saved for dynamic state space model
+%   -Ctp1: C(t-1) saved for dynamic state space model
 
     
     [n,d,T] = size(MeasMX);
     SmoothedMean = zeros(d, T);
     SmoothedCov = zeros(d,d,T);
+    Ctp1 = zeros(d,d,T);
 
     KFFit=KF(TranMX, DistMean, DistCov, MeasMX, ObseCov, ...
         data, StateMean0, StateCov0, false);
@@ -32,12 +36,13 @@ function KalmanFit = ...
     SmoothedCov(:,:,T) = KFFit.FilteredCov(:,:,T);
     
     for t=T-1:-1:1
-        [SmoothedMean(:,t), SmoothedCov(:,:,t)] = ...
-    KSUpdate(TranMX(:,:,t+1), ...
-            KFFit.FilteredMean(:,t), KFFit.FilteredCov(:,:,t), ...
-            KFFit.ForecastedMean(:,t+1), KFFit.ForecastedCov(:,:,t+1),...
-            SmoothedMean(:,t+1), SmoothedCov(:,:,t+1));
+        [SmoothedMean(:,t), SmoothedCov(:,:,t), Ctp1(:,:,t+1)] = ...
+            KSBackward(TranMX(:,:,t+1), ...
+                KFFit.FilteredMean(:,t), KFFit.FilteredCov(:,:,t), ...
+                KFFit.ForecastedMean(:,t+1), KFFit.ForecastedCov(:,:,t+1),...
+                SmoothedMean(:,t+1), SmoothedCov(:,:,t+1));
     end
+    Ctp1(:,:,1) = StateCov0*TranMX(:,:,1)'/KFFit.ForecastedCov(:,:,1);
     
     KalmanFit.loglik = KFFit.loglik;
     KalmanFit.ForecastedMean = KFFit.ForecastedMean;
@@ -46,6 +51,6 @@ function KalmanFit = ...
     KalmanFit.FilteredCov = KFFit.FilteredCov;
     KalmanFit.SmoothedMean = SmoothedMean;
     KalmanFit.SmoothedCov = SmoothedCov;
-    KalmanFit.C0 = StateCov0*TranMX(:,:,1)'/KFFit.ForecastedCov(:,:,1);
+    KalmanFit.Ctp1 = Ctp1;
 
 end
