cdd6c6d46516f5868ce0a79967e87ea8e748234f
Jialin Yi <jialinyi@sas.upenn.edu>
2017-07-26 17:08:29 -0400

    measurement for 200 subjects

--- Test/fme/newCondProb_test.m
+++ Test/fme/newCondProb_test.m
@@ -11,7 +11,7 @@ rng(1)                                       % control the randomness
 %% ********Testing for the fme example*********
 
 
-n = 20;                                       % number of subjects
+n = 200;                                       % number of subjects
 m = 30;                                       % number of observations
 t = (1:m)/m;
 p = 1;                                        % # of fixed effects
@@ -45,18 +45,19 @@ subj = n;
 
 ClusterData = Y(ClusterMem,:);
 subdata = Y(subj, :);
-tic
+tic;
 logCondProb = fmeCondProb(ClusterData, subdata, t, ...
                         fixedArray, randomArray, logpara0, diffusePrior)
-toc
+oldCondProb =toc;
 % compare to the result in DSSVSS1_R
 
 %% newCondProb
-tic
+tic;
 SSMArray = fme2ssArray(n, t, ...
-                       fixedArray, randomArray, logpara0, diffusePrior);s
+                       fixedArray, randomArray, logpara0, diffusePrior);
 
-generateArray = toc
+generateArray = toc;
 
+tic;
 logCondProb1 = fmeCondProb1(ClusterData, subdata, SSMArray(length(ClusterMem)+1))
-computingCondProb = toc
+newCondProb = toc;
