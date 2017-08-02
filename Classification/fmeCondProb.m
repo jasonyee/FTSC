d7ec8bc1ba89311af8ce46215a786ae908e12401
Jialin Yi <jialinyi@sas.upenn.edu>
2017-08-02 16:34:37 -0400

    rewrite fmeCondProb function

--- Classification/fmeCondProb.m
+++ Classification/fmeCondProb.m
@@ -1,12 +1,13 @@
-%% fmeCondProb
-%  fmeCondProb returns the density of a new subject
+%% CondProb
+%  CondProb returns the density of a new subject
 %  conditioning on the cluster in the functional mixed effect model
 
-function logCondProb = fmeCondProb(ClusterData, subdata, SSM, nFixedEffects, nRandomEffects)
+function logCondProb = fmeCondProb(Algo, ClusterData, subdata, SSMTotal, nFixedEffects, nRandomEffects)
 %Input: t=1:T
+%   -Algo: @DSSFull / @DSS2Step
 %   -ClusterData: (i,t) is the data of group member i at observation t.
 %   -subdata: (t) is the data of new subject at observation t.
-%   -SSM: the corresponding state-space model for ***one specific group*** 
+%   -SSMTotal: the total state-space model for all subjects
 %   and the dimension is for ***all*** subject
 %   -nFixedEffects: number of fixed effects
 %   -nRandomEffects: number of random effects
@@ -15,17 +16,20 @@ function logCondProb = fmeCondProb(ClusterData, subdata, SSM, nFixedEffects, nRa
 
     %  add the new subject to the last
     allData = [ClusterData; subdata];
-    [n, ~] = size(allData);
-    d = 2*(nFixedEffects + n*nRandomEffects);
-    %  fitting dss model
-    [KalmanFitCell, ~, ~] = dss_uni2step(SSM.TranMX(1:d, 1:d, :), ...
-                                         SSM.DistMean(1:d, :), ...
-                                         SSM.DistCov(1:d, 1:d, :), ...
-                                         SSM.MeasMX(1:n, 1:d, :), ...
-                                         SSM.ObseCov(1:n, 1:n, :), ...
-                                         allData, ...
-                                         SSM.StateMean0(1:d, :), ...
-                                         SSM.StateCov0(1:d, 1:d, :));
+    [nSubj, ~] = size(allData);
+    d = 2*(nFixedEffects + nSubj * nRandomEffects);
     
-    logCondProb = KalmanFitCell{end}.loglik;
+    %  construct new SSM
+    SSM.TranMX = SSMTotal.TranMX(1:d, 1:d, :);
+    SSM.DistMean = SSMTotal.DistMean(1:d, :);
+    SSM.DistCov = SSMTotal.DistCov(1:d, 1:d, :);
+    SSM.MeasMX = SSMTotal.MeasMX(1:nSubj, 1:d, :);
+    SSM.ObseCov = SSMTotal.ObseCov(1:nSubj, 1:nSubj, :);
+    SSM.StateMean0 = SSMTotal.StateMean0(1:d, :);
+    SSM.StateCov0 = SSMTotal.StateCov0(1:d, 1:d, :);
+    
+    %  fitting dss model
+    [~, KalmanArray] = Algo(SSM, allData);
+    
+    logCondProb = KalmanArray(end).loglik;
 end
