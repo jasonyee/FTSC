4117d01fb8ccd4d3f61a754905dc3c1d363fc7c3
Jialin Yi <jialinyi@sas.upenn.edu>
2017-08-04 21:26:17 -0400

    add comments for FME

--- FME/fmeTraining.m
+++ FME/fmeTraining.m
@@ -1,6 +1,24 @@
 function [logparahat, fval] = fmeTraining(Algo, Y, fixedArray, randomArray, t, logpara0, diffusePrior)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+%fmeTraining computes the MLEs
+% Description:
+%
+% estimate logpara by minimizing the negative log-likelihood value.
+%
+% Input Arguments:
+%   
+%   Algo - @BuiltIn / @KalmanAll / @DSS2Step / @DSSFull
+%   Y - n-by-T
+%   fixedArray - array for fixed effects
+%   randomArray - array for random effects
+%   t - time points, 1-by-T
+%   logpara: parameters for functional mixed effect model
+%   diffusePrior: a large value for diffusing prior
+%
+% Output Arguments:
+%
+%   logparahat - MLEs of logpara
+%   fval - minimized negative log-likelihood value
+
     Obj = @(logpara) ...
     NlogLik(Algo, Y, fixedArray, randomArray, t, logpara, diffusePrior);
 
