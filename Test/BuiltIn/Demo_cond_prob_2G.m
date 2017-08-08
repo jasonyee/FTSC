42dfedff6dfba9d98ee09c909bd186824568b4a0
Jialin Yi <jialinyi@sas.upenn.edu>
2017-08-08 09:53:12 -0400

    add sensitivity table and plot for the misclassified  subject

--- Test/BuiltIn/Demo_cond_prob_2G.m
+++ Test/BuiltIn/Demo_cond_prob_2G.m
@@ -1,4 +1,4 @@
-%% Test for fmeCond shuffled
+%% Test for fmeCond shuffled using VSS
 %  Adding the following folders to the path:
 %   -FTSC
 
@@ -39,9 +39,6 @@ realY2 = repmat(realFixedEffect2, [n2,1]) + realRandomEffect2;
 
 Y2 = realY2+ sqrt(sigma_e)*randn(n2,m);
 
-%% Shuffle data
-Y1 = Y1(randperm(size(Y1,1)),:);
-Y2 = Y2(randperm(size(Y2,1)),:);
 
 %% Model setting
 
@@ -70,6 +67,11 @@ fval = zeros(1, nClusters);
         fmeTraining(@BuiltIn, Y2, fixedArray, randomArray, t, logpara0, diffusePrior); 
 %%    
 logCondProb0 = - fval;
+
+%% Shuffle data
+% Y1 = Y1(randperm(size(Y1,1)),:);
+% Y2 = Y2(randperm(size(Y2,1)),:);
+
 %% Fitting SSM model
 % group 1
 SSMp1(1) = fme2ss(n1+1, fixedArray, randomArray, t, logparahat(:,1), diffusePrior);
@@ -85,6 +87,7 @@ logCondProb = zeros(n1+n2, nClusters);
 
 
 %% logCondProb for subjects from group 1
+correct_G1 = 0;
 for i=1:n1
     % compute the logcondprob for cluster 1
     Members1 = 1:n1;
@@ -93,9 +96,14 @@ for i=1:n1
     
     % compute the logcondprob for cluster 2
     logCondProb(i,2) = Algo(SSMp1(2), [Y2; Y1(i,:)]) - logCondProb0(2);
+    
+    if logCondProb(i,1) > logCondProb(i,2)
+        correct_G1 = correct_G1 + 1;
+    end
 end
 
 %% logCondProb for subjects from group 2
+correct_G2 = 0;
 for j=1:n2
     % compute the logcondprob for cluster 1
     logCondProb(n1+j,1) = Algo(SSMp1(1), [Y1; Y2(j,:)]) - logCondProb0(1);
@@ -104,6 +112,10 @@ for j=1:n2
     Members2 = 1:n2;
     Members2(Members2 == j) = [];
     logCondProb(n1+j,2) = logCondProb0(2) - Algo(SSMm1(2), Y2(Members2,:));
+    
+    if logCondProb(n1+j,1) < logCondProb(n1+j,2)
+        correct_G2 = correct_G2 + 1;
+    end
 end
 
 
@@ -126,3 +138,52 @@ end
 
 PosteriorsTable = ...
     array2table(round(Posteriors,4), 'VariableNames', VarNames, 'RowNames', RowNames)
+
+SensTable = array2table([correct_G1, n1 - correct_G1; n2 - correct_G2, correct_G2],...
+    'VariableNames', {'Cluster1', 'Cluster2'}, 'RowNames', {'Group1', 'Group2'})
+
+%% Group 1 state space model
+SSM_G1 = fme2ss(n1, fixedArray, randomArray, t, logparahat(:,1), diffusePrior);
+[logL_G1, Output_G1] = BuiltInSmoother(SSM_G1, Y1);
+
+%% Group 2 state space model
+SSM_G2 = fme2ss(n2, fixedArray, randomArray, t, logparahat(:,2), diffusePrior);
+[logL_G2, Output_G2] = BuiltInSmoother(SSM_G2, Y2);
+
+%% group average
+k = 1; % the real fixed effect state parameter
+ConfidenceLevel = 0.95; % confidence level
+% group 1
+[Smoothed_G1, SmoothedVar_G1] =...
+StatesMeanVar(Output_G1, 'built-in', 'smooth');
+[Smoothed95Upper_G1, Smoothed95Lower_G1] = ...
+NormalCI(Smoothed_G1, SmoothedVar_G1, ConfidenceLevel);
+% group 2
+[Smoothed_G2, SmoothedVar_G2] =...
+StatesMeanVar(Output_G2, 'built-in', 'smooth');
+[Smoothed95Upper_G2, Smoothed95Lower_G2] = ...
+NormalCI(Smoothed_G2, SmoothedVar_G2, ConfidenceLevel);
+
+r = 20;
+figure;
+plot(t, Y1(r,:),...
+t, Smoothed_G1(k,:),...
+t, Smoothed_G2(k,:),...
+t, Smoothed95Upper_G1(k,:), '--',...
+t, Smoothed95Lower_G1(k,:), '--',...
+t, Smoothed95Upper_G2(k,:), ':',...
+t, Smoothed95Lower_G2(k,:), ':');
+legend('raw', 'group 1', 'group 2');
+title(strcat('misclassified subhject, n=', num2str(r)));
+
+
+
+
+
+
+
+
+
+
+
+
