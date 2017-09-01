795bbdc43f3bcea9db922b1f6ba85896258ed0ab
Jialin Yi <jialinyi@sas.upenn.edu>
2017-08-31 21:02:31 -0400

    reset all

--- MATLAB built-in/fme2BuiltIn.m
+++
@@ -1,9 +0,0 @@
-function Md = fme2BuiltIn(n, fixedArray, randomArray, t, logpara, diffusePrior)
-%FME2BUILTIN
-%   functional mixed effect model -> SSM -> MATLAB's built-in ssm
-
-    SSM = fme2ss(n, fixedArray, randomArray, t, logpara, diffusePrior);
-    Md = ss2BuiltIn(SSM);
-    
-end
-
