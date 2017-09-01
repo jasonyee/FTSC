09d734494861c15a4b5ce6c240b401cb9ad18346
Jialin Yi <jialinyi@sas.upenn.edu>
2017-09-01 13:37:05 -0400

    spaghetti plot

--- Simulation/demo_simu.m
+++ Simulation/demo_simu.m
@@ -12,7 +12,7 @@ TrueMembers = ClusteringMembers(3, TrueID);
 
 %% Data I/O
 nSim = 1;
-NumC = 2;
+NumC = 3;
 
 path_result = 'Y:\Users\Jialin Yi\output\paper simulation\KL\result\';
 
@@ -42,12 +42,50 @@ SensTable(TrueMembers, WaldMembers)
 fprintf('State-space model clustering: \n')
 SensTable(TrueMembers, ClusterMembers)
 
-
 %% Switches plot
 plot(SwitchHistory);
 title(strcat('Switches when', {' '},...
         'nsim=', num2str(nSim), ',', {' '},...
         'nc=', num2str(NumC)));
+    
+%% Spaghetti plot with group average fit
+GrewPoints = .8 * ones(1,3);
+
+% get scale for dataset
+ymin = min(dataset(:));
+ymax = max(dataset(:));
+
+figure();
+for k=1:nClusters
+    
+    Y = ClusterData{k};
+    [n, T] = size(Y);
+    t = (1:T)/T;
+    
+    % Constructing ssm object
+    SSM_kalman{k} = ...
+        fmeRandomSinPriorBuiltIn(n, t, logparahat(:,k), diffusePrior);    
+    [~, logLikSmooth, Output] = smooth(SSM_kalman{k}, Y');
+    
+    [Smoothed, SmoothedVar] =...
+        StatesMeanVar(Output, 'built-in', 'smooth');
+
+    [Smoothed95Upper, Smoothed95Lower] = ...
+        NormalCI(Smoothed, SmoothedVar, ConfidenceLevel);
+    
+    subplot(1,nClusters,k);
+    plot(t, Y', 'Color', GrewPoints);
+    hold on;
+    plot(t, Smoothed(1,:),...
+        t, Smoothed95Upper(1,:), '--',...
+        t, Smoothed95Lower(1,:), '--');
+    hold on;
+    plot(t, zeros(1,T),'--');
+    hold off;
+    ylim([ymin, ymax]);
+    plottitle = strcat('Cluster', num2str(k), ' n=', num2str(n));
+    title(plottitle);
+end
 
 %% Subject-fit plotting
 nSubj = 9;
