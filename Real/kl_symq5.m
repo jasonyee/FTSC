f3642d9cba601e204f5095658242e8795a291645
Jialin Yi <jialinyi@sas.upenn.edu>
2017-09-03 12:52:10 -0400

    kl distance

---
+++ Real/kl_symq5.m
@@ -0,0 +1,39 @@
+%% Optimal number of clusters
+%  plot the Kullback-Leibler information distance curve
+%  For different dataset, change filename
+clear;
+clc;
+
+%% Simulation setting
+nSim = 1;
+nCL = 1;
+nCU = 10;
+d = nCU - nCL + 1;
+diffusePrior = 1e7;
+
+KL = @KLCondP ;
+
+%% preallocation
+KLD = zeros(1,d);
+
+%%  Computing the Kullback-Leibler distance for different clustering
+for NumC = nCL:nCU
+    
+    path_result = 'Y:\Users\Jialin Yi\output\SYMQ5\KL\';
+    
+    load(strcat(path_result, 'SYMQ5_dif_FC_', num2str(NumC),'C.mat'));
+    
+    q = NumC - nCL + 1;
+    
+    %KLD(q) = KL_equal(logP);
+    %KLD(q) = KL_CondP(logP);
+    KLD(q) = KL(logP);
+end
+
+%% KL distance curve and optimal number of clusters
+
+plot(KLD);
+[KL_opti, nclusters_opti] = min(KLD);
+text = strcat(func2str(KL),' information: The optimal number of clusters is', {' '}, num2str(nclusters_opti));
+title(text);
+
