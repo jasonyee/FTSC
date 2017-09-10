19f41b423081785aa305a201d9fa4f0b2b9dbdbf
Jialin Yi <jialinyi@sas.upenn.edu>
2017-09-10 17:46:44 -0400

    pain severity

---
+++ Real/compare_painsev.m
@@ -0,0 +1,54 @@
+%% PAINSEV: Compare KL curve using different algorithms to compute condtitional probabitlity
+clear;
+clc;
+
+%% Simulation setting
+nSim = 1;
+nCL = 1;
+nCU = 1;
+d = nCU - nCL + 1;
+diffusePrior = 1e7;
+
+KLCell = {@KL01, @KLCondP, @KLUnif};
+AlgoFlag = {'VSS', 'KPVSS', 'DSSFull'};
+
+BigKL = zeros(d,length(AlgoFlag), length(KLCell));
+
+%% Compute KL distances
+for NumC = nCL:nCU
+
+    path_result = 'Y:\Users\Jialin Yi\output\PAINSEV\Model Selection\';
+
+    load(strcat(path_result, 'PAINSEV_dif_FC_', num2str(NumC),'C.mat'));
+
+    q = NumC - nCL + 1;
+    
+    for ii=1:length(AlgoFlag)
+        logCP = ...
+            logCPWrapper(dataset, nClusters, ClusterIDs, ClusterMembers,...
+                logLik, logparahat,diffusePrior, AlgoFlag{ii});
+        for jj=1:length(KLCell)
+            KL = KLCell{jj};
+            BigKL(q,ii,jj) = KL(logCP);
+        end
+    end
+end
+
+%% Plotting
+f = figure;
+p = uipanel('Parent',f,'BorderType','none'); 
+p.Title = 'KL distance for PAINSEV';
+p.TitlePosition = 'centertop'; 
+p.FontSize = 12;
+p.FontWeight = 'bold';
+
+for jj=1:length(KLCell)
+    subplot(1,length(KLCell),jj,'Parent',p);
+    KLD = BigKL(:,:,jj);
+    plot(KLD);
+    legend(AlgoFlag{:});
+    %ylim([20,30]);
+    title(func2str(KLCell{jj}))
+end
+    
+    
---
+++ Real/ic_painsev.m
@@ -0,0 +1,54 @@
+%% PAINSEV: Optimal number of clusters
+%  plot the Akeika information criterion curve
+%  For different dataset, change filename
+clear;
+clc;
+
+%% Simulation setting
+nSim = 1;
+nCL = 1;
+nCU = 10;
+d = nCU - nCL + 1;
+diffusePrior = 1e7;
+
+ICCell = {@AIC, @BIC, @AICC};
+
+%% Plotting
+f = figure;
+p = uipanel('Parent',f,'BorderType','none'); 
+p.Title = 'Information Criterion for PAINSEV';
+p.TitlePosition = 'centertop'; 
+p.FontSize = 12;
+p.FontWeight = 'bold';
+
+
+for ii=1:length(ICCell)
+    
+    IC = ICCell{ii};
+    % preallocation
+    InfoCri = zeros(1,d);
+
+    %  Computing the information criterion for different clustering
+    for NumC = nCL:nCU
+
+        path_result = 'Y:\Users\Jialin Yi\output\PAINSEV\Model Selection\';
+
+        load(strcat(path_result, 'PAINSEV_dif_FC_', num2str(NumC),'C.mat'));
+
+        q = NumC - nCL + 1;
+        
+        if ii < 2
+            InfoCri(q) = IC(logLik, zeros(5, NumC));
+        else
+            InfoCri(q) = IC(logLik, zeros(5, NumC), 397);
+        end
+    end
+
+    % Information criterion curve and optimal number of clusters
+    subplot(1,length(ICCell),ii,'Parent',p)
+    plot(InfoCri);
+    [IC_opti, nclusters_opti] = min(InfoCri);
+    text = strcat(func2str(IC),' : The optimal number of clusters is', {' '}, num2str(nclusters_opti));
+    title(text);
+    ylim([3e4,3.5e4]);
+end
