3f8b39cfd076e2cb2cdd5285995d4f21326a3aba
Jialin Yi <jialinyi@sas.upenn.edu>
2017-10-11 21:24:50 -0400

    kronecker product

--- Simulation/SYMQ5FixSimulation.m
+++ Simulation/SYMQ5FixSimulation.m
@@ -1,52 +1,53 @@
 clear;clc;
 % Generating data
+% fix random seed
+rng(1);
+load('Y:\Users\Jialin Yi\output\SYMQ5\MATLAB\C3\FixedEffect.mat');
 group_size = 20;
-nsamples = 3*group_size;
 var_random = 9;
 var_noise = 3;
-T = 23;
+
+[nGroup, T] = size(FixedEffect);
+nsamples = nGroup*group_size;
 t = (1:T)/T;
 MAX_LOOP = 20;
 logpara0 = [0.5;6;6;-5;0];
 
-% fix random seed
-rng(1);
 
 % random effect
 RandomEffect = func_dev(nsamples, t, var_random);
 % white noise
 WhiteNoise = sqrt(var_noise)*randn(nsamples, T);
 % fixed effect
-load('Y:\Users\Jialin Yi\output\SYMQ5\MATLAB\C3\FixedEffect.mat');
 SampleFixedEffect = kron(FixedEffect, ones(group_size,1));
                  
 % Truth                 
 Y = SampleFixedEffect + RandomEffect + WhiteNoise;
-TrueID = [ones(group_size,1); 2*ones(group_size,1); 3*ones(group_size,1)];
-TrueMemebers = ClusteringMembers(3, TrueID);
+TrueID = kron((1:nGroup)', ones(group_size,1));
+TrueMemebers = ClusteringMembers(nGroup, TrueID);
 
 % kmeans
-kmeansID = kmeans(Y,3);
-kmeansMembers = ClusteringMembers(3, kmeansID);
+kmeansID = kmeans(Y,nGroup);
+kmeansMembers = ClusteringMembers(nGroup, kmeansID);
 
-kmeans3by3 = table2array(SensTable(TrueMemebers, kmeansMembers));
+kmeans_nbyn = table2array(SensTable(TrueMemebers, kmeansMembers));
 
-kmeans_CRate = mean(max(kmeans3by3, [], 2))/group_size;
+kmeans_CRate = mean(max(kmeans_nbyn, [], 2))/group_size;
 
-[~, kmeans_GroupNum] = max(kmeans3by3);
+[~, kmeans_GroupNum] = max(kmeans_nbyn);
 
-kmeans_isSeparated = (length(unique(kmeans_GroupNum)) == 3);
+kmeans_isSeparated = (length(unique(kmeans_GroupNum)) == nGroup);
 
 % FTSC
 IniClusterIDs = kmeansID;
-nClusters = 3;
+nClusters = nGroup;
 [ClusterIDs, ClusterMembers, SwitchHistory, logparahat, logP, logLik] =...
     SSMBuiltInClustering(Y, nClusters, IniClusterIDs, logpara0, MAX_LOOP);
 
-FTSC3by3 = table2array(SensTable(TrueMemebers, ClusterMembers));
+FTSC_nbyn = table2array(SensTable(TrueMemebers, ClusterMembers));
 
-FTSC_CRate = mean(max(FTSC3by3, [], 2))/group_size;
+FTSC_CRate = mean(max(FTSC_nbyn, [], 2))/group_size;
 
-[~, FTSC_GroupNum] = max(FTSC3by3);
+[~, FTSC_GroupNum] = max(FTSC_nbyn);
 
-FTSC_isSeparated = (length(unique(FTSC_GroupNum)) == 3);
+FTSC_isSeparated = (length(unique(FTSC_GroupNum)) == nGroup);
