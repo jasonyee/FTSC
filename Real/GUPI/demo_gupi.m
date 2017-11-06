59cd5f25149a2cb6f78465e518103954235f615e
Jialin Yi <jialinyi@sas.upenn.edu>
2017-11-06 13:39:43 -0500

    GUPI_qssymfeel

--- Real/GUPI/demo_gupi.m
+++ Real/GUPI/demo_gupi.m
@@ -6,12 +6,29 @@
 clear;
 clc;
 
+yvar = 'gupi';
+YVAR_path = 'GUPI';
+YVAR_plot = 'Gupi_qssymfeel';
+
+%% K=1 Spaghetti Plot
+
+k1_path = strcat('Y:\Users\Jialin Yi\data\imputation\', yvar, '\');
+load(strcat(k1_path, yvar, '_3dif.mat'))
+
+figure;
+plot(Threedif');
+ylim([min(Threedif(:))-1, max(Threedif(:))+1])
+xlim([0, 26])
+title(strcat('Longitudinal', {' '}, YVAR_plot, ': Change from Week 4 (vnum=3)'));
+ylabel('Change from vnum=3')
+xlabel('vnum')
+
 %% Data I/O
 NumC = 3;
 
-path_result = 'Y:\Users\Jialin Yi\output\GUPI\Model Selection\';
+path_result = strcat('Y:\Users\Jialin Yi\output\', YVAR_path, '\Model Selection\');
 
-load(strcat(path_result, 'GUPI_dif_FC_', num2str(NumC),'C.mat'));
+load(strcat(path_result, YVAR_path,'_dif_FC_', num2str(NumC),'C.mat'));
 
 %% clustering running time
 fprintf('The clustering algorithm running time is %.2f minutes.\n', clustertime/60)
@@ -34,9 +51,10 @@ fprintf('Functional clustering v.s. kmeans: \n')
 SensTable(WaldMembers, ClusterMembers)
 
 %% Switches plot
+figure;
 plot(SwitchHistory);
-title(strcat('Switches when', {' '},...
-        'GUPI', {', '}, ...
+title(strcat('Swaps in iterations for ', {' '},...
+        YVAR_plot, {', '}, ...
         'nClusters=', num2str(nClusters)));
     
 %% Spaghetti plot with group average fit
@@ -49,7 +67,7 @@ ymax = max(dataset(:))+1;
 f = figure;
 p = uipanel('Parent',f,'BorderType','none'); 
 p.Title = strcat('Spaghetti plot for', {' '}, ...
-                'GUPI', {', '}, ...
+                YVAR_plot, {', '}, ...
                 'nClusters=',num2str(nClusters)); 
 p.TitlePosition = 'centertop'; 
 p.FontSize = 12;
@@ -74,13 +92,16 @@ for k=1:nClusters
     subplot(1,nClusters,k,'Parent',p);
     plot(t, Y', 'Color', GrewPoints);
     hold on;
-    plot(t, Smoothed(1,:),...
-        t, Smoothed95Upper(1,:), '--',...
-        t, Smoothed95Lower(1,:), '--');
+    plot(t, Smoothed(1,:), 'Color', [0;0;156]/255, 'LineWidth', 1.3)
+    plot(t, Smoothed95Upper(1,:), 'LineStyle', '--', 'Color', [192;0;0]/255, 'LineWidth', 0.8)
+    plot(t, Smoothed95Lower(1,:), 'LineStyle', '--', 'Color', [192;0;0]/255, 'LineWidth', 0.8);
     hold on;
-    plot(t, zeros(1,T),'--');
+    plot(t, zeros(1,T),'-- k');
     hold off;
     ylim([ymin, ymax]);
+    if k== 1 
+        ylabel('Change from Week 4 (vnum = 3)'); 
+    end
     plottitle = strcat('Cluster', num2str(k), ' n=', num2str(n));
     title(plottitle);
 end
@@ -105,7 +126,7 @@ for k=1:nClusters
     
     f = figure;
     p = uipanel('Parent',f,'BorderType','none'); 
-    p.Title = strcat('GUPI: subjects in Cluster',num2str(k)); 
+    p.Title = strcat(YVAR_plot, ': subject-fit in Cluster',num2str(k)); 
     p.TitlePosition = 'centertop'; 
     p.FontSize = 12;
     p.FontWeight = 'bold';
