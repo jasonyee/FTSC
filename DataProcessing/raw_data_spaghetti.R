4edab250066c17cc20bca01fe43993a720906562
Jialin Yi <jialinyi@sas.upenn.edu>
2017-11-07 12:52:08 -0500

    raw data spaghetti

---
+++ DataProcessing/raw_data_spaghetti.R
@@ -0,0 +1,18 @@
+library(sas7bdat)
+library(R.matlab)
+
+yvar = "gupisub"
+
+path <- paste("Y:/Users/Jialin Yi/data/imputation/", yvar, "/", sep = "")
+
+name.sas <- paste(yvar, ".sas7bdat", sep = "")
+
+raw <- read.sas7bdat(paste(path, name.sas,sep = ""))
+
+yvar_dif <- raw[,4]
+
+yvar_mat <- matrix(yvar_dif, nrow = 397, ncol = 25, byrow = TRUE)
+
+result_name = paste(path, yvar, "_3dif.mat", sep = "")
+
+writeMat(result_name, Threedif = yvar_mat)
