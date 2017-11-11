4402adee6cee07864034e37588f00b9f9eac511f
Jialin Yi <jialinyi@sas.upenn.edu>
2017-11-10 23:07:06 -0500

    heterogenous

--- Simulation/demo_linux_simu.m
+++ Simulation/demo_linux_simu.m
@@ -1,17 +1,20 @@
-%% Homogeneous Random Effects, multiple simulations
+%% Heterogeneous Random Effects, multiple simulations
 clear;clc;
 
-sample_sim = 24;
+sample_sim = 30;
 
 nSim = 50;
 group_size = 100;
-var_random = 100;
-var_noise = 1;
+var_random1 = 50;
+var_random2 = 200;
+var_random3 = 100;
+var_noise = 2;
 
-path_result = 'Y:\Users\Jialin Yi\output\paper simulation\FixNClusters\data\';
+path_result = 'Y:\Users\Jialin Yi\output\paper simulation\VaryClusters\data\';
 
 load(strcat(path_result, num2str(nSim),'-', num2str(group_size),'-',...
-    num2str(var_random),'-', num2str(var_noise), '.mat'));
+    num2str(var_random1),'-', num2str(var_random2),'-',...
+    num2str(var_random3),'-',num2str(var_noise), '.mat'));
 
 %% Spaghetti plot with group average fit
 nClusters = size(FixedEffect,1);
