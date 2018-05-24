f1ea714bf08238a4bc061999931c8bbfb6c44ede
Jialin Yi <jialinyi@sas.upenn.edu>
2018-05-24 18:51:40 -0400

    Labels for first period clustering

--- Real/GetLabels.m
+++ Real/GetLabels.m
@@ -2,7 +2,9 @@ clear;clc;
 
 filepath = 'Y:\Users\Jialin Yi\output\Labels\';
 
-realdata = {'symq5','symq6','symq7','symq8','painsev','urinsev','gupi', 'gupisub', 'sf12_mcs', 'sf12_pcs'};
+realdata = {'symq5','symq6','symq7','symq8','painsev','urinsev',...
+            'gupi', 'gupisub', 'sf12_mcs', 'sf12_pcs', 'painsev4to24',...
+            'urinsev4to24'};
 
 n = 397;
 
