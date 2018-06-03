ed20973adfa55089c429e6ebf6d4a63021053424
Jialin Yi <jialinyi@sas.upenn.edu>
2018-06-03 19:30:43 -0400

    Urinsev

---
+++ Real/URINSEV/benchmark/clusterplot_urinsev.m
@@ -0,0 +1,26 @@
+%%  URINSEV: Spaghetti plot with ClusterIds
+%   change the truth id accordingly when analyzing different simulations
+%   -FTSC
+
+%% loading clustering resulss
+clear;
+clc;
+
+% change Options for different clustering result
+Options = 'Model Selection';
+
+yvar = 'urinsev';
+YVAR_path = 'URINSEV';
+YVAR_plot = 'Urinary Severity';
+
+%% Data I/O: path_result locates the clustering result output by FTSC.
+NumC = 3;
+
+path_result = strcat('Y:\Users\Jialin Yi\output\', YVAR_path, '\', Options);
+
+file_path = strcat(path_result, '\', YVAR_path,'_dif_FC_', num2str(NumC),'C.mat');
+
+cluster_struct = load(file_path);
+%% Plotting
+
+ClusterPlot(cluster_struct, YVAR_plot)
