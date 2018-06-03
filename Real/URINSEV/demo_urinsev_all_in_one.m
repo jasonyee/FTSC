de434207df294652b7b610fa9a058ddc1384982a
Jialin Yi <jialinyi@sas.upenn.edu>
2018-06-03 14:05:47 -0400

    save changes

--- Real/URINSEV/demo_urinsev_all_in_one.m
+++ Real/URINSEV/demo_urinsev_all_in_one.m
@@ -1,4 +1,4 @@
-%%  Urin severity: Sensitivity analysis, switches plot and random subject fit plotting
+%%  URINSEV: Sensitivity analysis, switches plot and random subject fit plotting
 %   change the truth id accordingly when analyzing different simulations
 %   -FTSC
 
@@ -6,12 +6,20 @@
 clear;
 clc;
 
+yvar = 'urinsev';
+YVAR_path = 'URINSEV';
+YVAR_plot = 'Urinary Severity FTSC';
+target = 'AllinOne';
+
+subplot_order = {'0-improved', '1-stable', '2-worse'};
+
+cluster_id_progress = [2, 3, 1]; % improved clusters: 1st arguement (cluster2)
 %% Data I/O
 NumC = 3;
 
-path_result = 'Y:\Users\Jialin Yi\output\URINSEV\Model Selection\';
+path_result = strcat('Y:\Users\Jialin Yi\output\', YVAR_path, '\', target);
 
-load(strcat(path_result, 'URINSEV_dif_FC_', num2str(NumC),'C.mat'));
+load(strcat(path_result, '\', YVAR_path, '_dif_F_', num2str(NumC),'C.mat'));
 
 %% clustering running time
 fprintf('The clustering algorithm running time is %.2f minutes.\n', clustertime/60)
@@ -25,21 +33,16 @@ SSM_kalman = cell(1, nClusters);
 diffusePrior = 1e7;
 ConfidenceLevel = 0.95;     % confidence level
 
-%% sensitivity analysis
-
-% wald's minimum variance
-WaldMembers = ClusteringMembers(nClusters, WaldClusterID);
-% state-space model clustering
-fprintf('Functional clustering v.s. kmeans: \n')
-SensTable(WaldMembers, ClusterMembers)
-
 %% Switches plot
+figure;
 plot(SwitchHistory);
-title(strcat('Switches when', {' '},...
-        'URINSEV', {', '}, ...
+title(strcat('Swaps in iterations for ', {' '},...
+        YVAR_plot, {', '}, ...
         'nClusters=', num2str(nClusters)));
     
 %% Spaghetti plot with group average fit
+random_num = 0;
+
 GrewPoints = .8 * ones(1,3);
 
 % get scale for dataset
@@ -49,12 +52,14 @@ ymax = max(dataset(:))+1;
 f = figure;
 p = uipanel('Parent',f,'BorderType','none'); 
 p.Title = strcat('Spaghetti plot for', {' '}, ...
-                'URINSEV', {', '}, ...
+                YVAR_plot, {', '}, ...
                 'nClusters=',num2str(nClusters)); 
 p.TitlePosition = 'centertop'; 
 p.FontSize = 12;
 p.FontWeight = 'bold';
-for k=1:nClusters
+for m=1:nClusters
+    
+    k = cluster_id_progress(m); % the cluster that will shown in position m.
     
     Y = ClusterData{k};
     [n, T] = size(Y);
@@ -71,17 +76,26 @@ for k=1:nClusters
     [Smoothed95Upper, Smoothed95Lower] = ...
         NormalCI(Smoothed, SmoothedVar, ConfidenceLevel);
     
-    subplot(1,nClusters,k,'Parent',p);
+    if random_num
+        Y = datasample(Y, random_num, 'Replace', false);
+    end
+    
+    
+    subplot(1,nClusters,m,'Parent',p);
+    
     plot(t, Y', 'Color', GrewPoints);
     hold on;
-    plot(t, Smoothed(1,:),...
-        t, Smoothed95Upper(1,:), '--',...
-        t, Smoothed95Lower(1,:), '--');
+    plot(t, Smoothed(1,:), 'Color', [0;0;156]/255, 'LineWidth', 1.3)
+    plot(t, Smoothed95Upper(1,:), 'LineStyle', '--', 'Color', [192;0;0]/255, 'LineWidth', 0.8)
+    plot(t, Smoothed95Lower(1,:), 'LineStyle', '--', 'Color', [192;0;0]/255, 'LineWidth', 0.8);
     hold on;
-    plot(t, zeros(1,T),'--');
+    plot(t, zeros(1,T),'-- k');
     hold off;
     ylim([ymin, ymax]);
-    plottitle = strcat('Cluster', num2str(k), ' n=', num2str(n));
+    if m== 1 
+        ylabel('Change from Week 4 (vnum = 3)'); 
+    end
+    plottitle = strcat(subplot_order{m}, '(', ' n=', num2str(n), ')');
     title(plottitle);
 end
 
@@ -105,10 +119,21 @@ for k=1:nClusters
     
     f = figure;
     p = uipanel('Parent',f,'BorderType','none'); 
-    p.Title = strcat('URINSEV: subjects in Cluster',num2str(k)); 
+    p.Title = strcat(YVAR_plot, ': subject-fit in Cluster',num2str(cluster_id_progress(k))); 
     p.TitlePosition = 'centertop'; 
     p.FontSize = 12;
     p.FontWeight = 'bold';
 
     RandomSubjFitBuiltIn(nSubj, Y, Members, SSM_kalman{k}, Output_builtin, [ymin, ymax], p);
 end
+
+% sensitivity analysis
+FTSCClusterMembers = ClusterMembers;
+path_result = strcat('Y:\Users\Jialin Yi\output\', YVAR_path, '\Model Selection\');
+load(strcat(path_result, YVAR_path,'_dif_FC_', num2str(NumC),'C.mat'), '');
+
+SensTable = ThreeCatSensPlot(FTSCClusterMembers, cluster_id_progress, 'FTSC_',...
+    ClusterMembers, [3, 2, 1], 'all_');
+
+uitable('Data',SensTable{:,:},'ColumnName',SensTable.Properties.VariableNames,...
+    'RowName',SensTable.Properties.RowNames,'Units', 'Normalized', 'Position',[0, 0, 1, 1]);
