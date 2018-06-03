7180093e7ec489092e331e4fcc9507b17f06cfd2
Jialin Yi <jialinyi@sas.upenn.edu>
2018-06-03 17:19:44 -0400

    panel plot with raw cluster ids

--- Real/PAINSEV/plotting/clusterplot_painsev.m
+++ Real/PAINSEV/plotting/clusterplot_painsev.m
@@ -6,7 +6,8 @@
 clear;
 clc;
 
-Options = 'AllinOne';
+% change Options for different clustering result
+Options = 'Model Selection';
 
 yvar = 'painsev';
 YVAR_path = 'PAINSEV';
@@ -17,72 +18,9 @@ NumC = 3;
 
 path_result = strcat('Y:\Users\Jialin Yi\output\', YVAR_path, '\', Options);
 
-load(strcat(path_result, '\', YVAR_path,'_dif_FC_', num2str(NumC),'C.mat'));
+file_path = strcat(path_result, '\', YVAR_path,'_dif_FC_', num2str(NumC),'C.mat');
 
-%% clustering running time
-fprintf('The clustering algorithm running time is %.2f minutes.\n', clustertime/60)
+cluster_struct = load(file_path);
+%% Plotting
 
-%% preallocation
-% get data in each cluster
-ClusterData = ClusteringData(dataset, ClusterMembers);
-% SSMTotal is a cell array of SSM for all batch data
-SSM_kalman = cell(1, nClusters);
-
-diffusePrior = 1e7;
-ConfidenceLevel = 0.95;     % confidence level
-    
-%% Spaghetti plot with group average fit
-random_num = 0;
-
-GrewPoints = .8 * ones(1,3);
-
-% get scale for dataset
-ymin = min(dataset(:))-1;
-ymax = max(dataset(:))+1;
-
-f = figure;
-p = uipanel('Parent',f,'BorderType','none'); 
-p.Title = strcat('Raw-ClusterIDs plot for', {' '}, ...
-                YVAR_plot, {', '}, ...
-                'nClusters=',num2str(nClusters)); 
-p.TitlePosition = 'centertop'; 
-p.FontSize = 12;
-p.FontWeight = 'bold';
-for k=1:nClusters
-    
-    Y = ClusterData{k};
-    [n, T] = size(Y);
-    t = (1:T)/T;
-    
-    % Constructing ssm object
-    SSM_kalman{k} = ...
-        fmeRandomSinPriorBuiltIn(n, t, logparahat(:,k), diffusePrior);    
-    [~, logLikSmooth, Output] = smooth(SSM_kalman{k}, Y');
-    
-    [Smoothed, SmoothedVar] =...
-        StatesMeanVar(Output, 'built-in', 'smooth');
-
-    [Smoothed95Upper, Smoothed95Lower] = ...
-        NormalCI(Smoothed, SmoothedVar, ConfidenceLevel);
-    
-    if random_num
-        Y = datasample(Y, random_num, 'Replace', false);
-    end
-    
-    subplot(1,nClusters,k,'Parent',p);
-    
-    plot(t, Y', 'Color', GrewPoints);
-    hold on;
-    plot(t, Smoothed(1,:), 'Color', [0;0;156]/255, 'LineWidth', 1.3)
-    plot(t, Smoothed95Upper(1,:), 'LineStyle', '--', 'Color', [192;0;0]/255, 'LineWidth', 0.8)
-    plot(t, Smoothed95Lower(1,:), 'LineStyle', '--', 'Color', [192;0;0]/255, 'LineWidth', 0.8);
-    hold on;
-    plot(t, zeros(1,T),'-- k');
-    hold off;
-    ylim([ymin, ymax]);
-    if k== 1 
-        ylabel('Change from Week 4 (vnum = 3)'); 
-    end
-    plottitle = strcat('Cluster', num2str(k), ' n=', num2str(n));
-    title(plottitle);
-end
+ClusterPlot(cluster_struct, YVAR_plot)
