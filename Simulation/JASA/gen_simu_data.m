30eb08f968bcba29360ab271447644800bfa951e
Jialin Yi <jialinyi@sas.upenn.edu>
2018-06-08 22:23:42 -0400

    simulation data
    10-100-100-100-100-1/2

---
+++ Simulation/JASA/gen_simu_data.m
@@ -0,0 +1,9 @@
+clear;clc;
+Path_FEffect = 'Y:\Users\Jialin Yi\output\SYMQ5\MATLAB\C3\';
+Path_OutputData = 'Y:\Users\Jialin Yi\output\paper simulation\temp\data\';
+
+% paras = [10,100,100,100,100,1];
+% [file_name] = sim_data(Path_FEffect, Path_OutputData, paras);
+
+paras = [10,100,100,100,100,2];
+[file_name] = sim_data(Path_FEffect, Path_OutputData, paras);
